713e6e7b1086c2ad219a5cf584f39ef5cd3504ce
Cyliana <a.taddei@codeur.online>
2021-09-09 21:15:44 +0200

    first commit

---
+++ server.php
@@ -0,0 +1,18 @@
+<?php
+// Entraînement:
+// Tu créés un table sur un fichier PHP dans lequel tu stockeras des noms de personnes.
+// Ensuite, tu fais en sortes d'envoyers ce TABLEAU vers un fichier JS.
+// Et tu traîtes cellui-ci pour afficher le résultat sur le navigateur.
+
+
+
+
+
+
+
+
+
+
+
+
+?>
